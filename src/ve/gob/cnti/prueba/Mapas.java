e69fb083a37ff200ae2e3b5e5100bef34f4f5adf
EhisonBP <ehisonbp@gmail.com>
2013-05-09 17:05:32 -0430

    Agregando metodo para especificar punto en el mapa

--- src/ve/gob/cnti/prueba/Mapas.java
+++ src/ve/gob/cnti/prueba/Mapas.java
@@ -1,6 +1,6 @@
 package ve.gob.cnti.prueba;
 
-import org.osmdroid.views.MapController;
+import org.osmdroid.util.GeoPoint;
 import org.osmdroid.views.MapView;
 
 import android.app.Activity;
@@ -9,17 +9,26 @@ import android.os.Bundle;
 public class Mapas extends Activity {
 
 	private MapView mapView;
-	private MapController controller;
+
+	// private MapController controller;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.mapas);
-		mapView = (MapView) findViewById(R.id.openmapview);
+		/**
+		 * mapView = (MapView) findViewById(R.id.openmapview);
+		 * mapView.setBuiltInZoomControls(true);
+		 * mapView.setMultiTouchControls(true); controller =
+		 * mapView.getController(); controller.setZoom(5);
+		 */
+		mapView = new MapView(this, 256);
 		mapView.setBuiltInZoomControls(true);
 		mapView.setMultiTouchControls(true);
-		controller = mapView.getController();
-		controller.setZoom(4);
+
+		mapView.getController().setZoom(10);
+		mapView.getController().setCenter(new GeoPoint(39.461078, 2.856445));
+		setContentView(mapView);
 	}
 
 }
